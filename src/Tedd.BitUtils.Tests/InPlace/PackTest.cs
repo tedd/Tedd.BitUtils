e11fc10ef32b3d5aa0c8dd8e0e9dfcf8eb58a483
Tedd Hansen <tedd@konge.net>
2019-11-08 10:00:07 +0100

    PackTest (incomplete)

---
+++ src/Tedd.BitUtils.Tests/InPlace/PackTest.cs
@@ -0,0 +1,53 @@
+using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Tedd.BitUtils.Tests.InPlace
+{
+    public class PackTest
+    {
+        private readonly ITestOutputHelper _output;
+        private Random _rnd = new Random();
+        public PackTest(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        private Int32 GetRandomInt32() => _rnd.Next(Int32.MinValue, Int32.MaxValue);
+        private UInt32 GetRandomUInt32() => (UInt32)GetRandomInt32();
+        private Int64 GetRandomInt64() => GetRandomInt32() | (GetRandomInt32() << sizeof(Int32));
+        private UInt64 GetRandomUInt64() => (UInt64)(((UInt64)GetRandomInt32()) | (((UInt64)GetRandomInt32()) << sizeof(Int32)));
+
+
+        //[Fact]
+        //public void TestInt32()
+        //{
+        //    var max = 1000;
+        //    for (var offset = 0; offset < sizeof(Int32) * 8; offset++)
+        //    {
+        //        for (var len = 0; len < sizeof(Int32) * 8; len++)
+        //        {
+        //            // for (var r = 0; r < 1000; r++)
+        //            {
+        //                var a = GetRandomInt32();
+        //                var cm1 = ((~0U << offset) >> (offset + len)) << len;
+        //                //_output?.WriteLine(Convert.ToString((Int32)cm1, 2).PadLeft(sizeof(Int32) * 8, '0') + "  & " + Convert.ToString((Int32)cm2, 2).PadLeft(sizeof(Int32) * 8, '0'));
+        //                // Clear space for val
+        //                a &= ~(Int32)cm1;
+
+        //                var val = (UInt32)GetRandomInt32();
+        //                var valResult = val | (~0U >> sizeof(Int32) * 8 - len);
+        //                var v = a;
+        //                a |= (Int32)(valResult << offset);
+        //                v.Pack(offset, len, (UInt32)val);
+
+        //                _output?.WriteLine(Convert.ToString((Int32)a, 2).PadLeft(sizeof(Int32) * 8, '0') + " == " + Convert.ToString((Int32)v, 2).PadLeft(sizeof(Int32)*8, '0'));
+
+        //                if (--max < 0)
+        //                    return;
+        //            }
+        //        }
+        //    }
+        //}
+    }
+}
